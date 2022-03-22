a9e03059f12d0db529febc061d1666b2024a4a18
grantaspetraitis <grantas157@gmail.com>
2022-03-22 20:59:19 +0200

    1 uzduoties update

--- Task 1/script.js
+++ Task 1/script.js
@@ -18,11 +18,19 @@ outputas.classList.add("nerodyti");
 
 forma.addEventListener("submit", e => {
     e.preventDefault();
+    if(inputas.value <= 0){
+        outputas.innerHTML = `<h2>Prašome įvesti tinkamą skaičių</h2>`;
+    } else {
     let svarai = inputas.value * 2.2046;
     let gramai = inputas.value / 0.001;
     let uncijos = inputas.value * 35.274;
     outputas.classList.remove("nerodyti");
-    outputas.innerHTML += `<h3>Jūsų svoris svarais: ${svarai} lbs</h3>`;
-    outputas.innerHTML += `<h3>Jūsų svoris gramais: ${gramai} g</h3>`;
-    outputas.innerHTML += `<h3>Jūsų svoris uncijomis: ${uncijos} oz</h3>`;
+    outputas.innerHTML = `  <div>
+                                <h3>Jūsų svoris svarais: ${svarai} lbs</h3>
+                                <h3>Jūsų svoris gramais: ${gramai} g</h3>
+                                <h3>Jūsų svoris uncijomis: ${uncijos} oz</h3>
+                            </div>`;
+    }
 });
+
+//Tik vakare prisedus pastebejau, kad ne visai gerai buvau parases koda. Dar pridejau salyga, bet man ji kazkodel keistai veikia.
